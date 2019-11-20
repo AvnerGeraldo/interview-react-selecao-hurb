f4e861a73eff12a2e976afac9d863055221a3708
Avner <avner.conceicao@hotmail.com>
2019-11-20 18:33:00 -0300

    Criando reducer

--- src/app/reducer/index.js
+++ src/app/reducer/index.js
@@ -1,9 +1,23 @@
-const initialState = {
+//Actions
+import { SET_WEATHER_DATA, FAIL_SET_WEATHER_DATA } from '../actions'
 
+const initialState = {
+	data: {},
+	error: ''
 }
 
 export default (state = initialState, { type, payload }) => {
-	switch (type) {	
+	switch (type) {
+		case SET_WEATHER_DATA:
+			return {
+				data: payload.data,
+				error: ''
+			}
+		case FAIL_SET_WEATHER_DATA:
+			return {
+				data: {},
+				error: payload.error
+			}
 		default:
 			return state
 	}
