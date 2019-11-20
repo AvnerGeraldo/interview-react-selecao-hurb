ef3d48f13bd73d9e35d9903f25c3ad8f7d765815
Avner <avner.conceicao@hotmail.com>
2019-11-20 11:12:49 -0300

    Criando tela principal

---
+++ src/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//Css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Bootstrap
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default _=> (
+	<Container fluid>
+		<Row className="justify-content-center">
+			<Col md="auto" lg="4" xs="6">
+				
+			</Col>
+		</Row>
+	</Container>
+)
