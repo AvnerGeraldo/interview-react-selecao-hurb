923b6a002eea0fd1b6ad9f2b96aa95fd3059aa2b
Avner <avner.conceicao@hotmail.com>
2019-11-20 18:31:10 -0300

    Criando busca WeatherAPI

---
+++ src/api/WeatherAPI.js
@@ -0,0 +1,29 @@
+import moment from 'moment'
+
+const url = "http://api.openweathermap.org/data/2.5/forecast"
+const token = "7ba73e0eb8efe773ed08bfd0627f07b8"
+
+export const getWeatherByCity = async (city, units = 'metric') => {
+	try {
+		let endpoint = `${url}?q=${encodeURIComponent(city)}&APPID=${token}&lang=pt`
+		endpoint += `&units=${units}`
+		const res = await fetch(endpoint)
+		const data = await res.json()
+
+		if (data) {
+			const actualHour = moment(data.list[0].dt_txt).format('HH:mm:ss')
+			
+			//Filter data with starter hour
+			data.list = data.list.filter(i => actualHour === moment(i.dt_txt).format('HH:mm:ss'))
+				.filter((i, index) => index < 3)
+		}
+
+		return {
+			data
+		}
+	} catch(e) {
+		return {
+			error: e.message
+		}
+	}
+}
