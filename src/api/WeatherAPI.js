74e82c3fdce92eca2ec1d3c8028c8122bc22eb7a
Avner <avner.conceicao@hotmail.com>
2019-11-20 22:03:44 -0300

    Ajustando retorno de erro da API

--- src/api/WeatherAPI.js
+++ src/api/WeatherAPI.js
@@ -7,20 +7,19 @@ export const getWeatherByCity = async (city, units = 'metric') => {
 	try {
 		let endpoint = `${url}?q=${encodeURIComponent(city)}&APPID=${token}&lang=pt`
 		endpoint += `&units=${units}`
-		const res = await fetch(endpoint)
-		const data = await res.json()
+		const response = await fetch(endpoint)
+		const data = await response.json()
 
-		if (data) {
-			const actualHour = moment(data.list[0].dt_txt).format('HH:mm:ss')
-			
-			//Filter data with starter hour
-			data.list = data.list.filter(i => actualHour === moment(i.dt_txt).format('HH:mm:ss'))
-				.filter((i, index) => index < 3)
-		}
+		if (data.message)
+			throw new Error(data.message)
 
-		return {
-			data
-		}
+		const actualHour = moment(data.list[0].dt_txt).format('HH:mm:ss')
+		
+		//Filter data with starter hour
+		data.list = data.list.filter(i => actualHour === moment(i.dt_txt).format('HH:mm:ss'))
+			.filter((i, index) => index < 3)
+
+		return { ...data }
 	} catch(e) {
 		return {
 			error: e.message
