1e8069867e024a2f71ecd0caa34c82140b930434
Avner <avner.conceicao@hotmail.com>
2019-11-21 00:09:47 -0300

    Ajustando o estilo do container principal na index

--- src/app/index.js
+++ src/app/index.js
@@ -4,18 +4,31 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Bootstrap
-import { Container, Row, Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 //Components
 import AppConfig from './config'
 import SearchText from './components/SearchText/SearchText'
+import WeatherBox from './components/WeatherBox/WeatherBox'
+
+//styled
+import styled from 'styled-components'
+
+const Container = styled.div`
+	min-height: 100vh;
+    min-width: 50vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
 
 export default _=> (
 	<AppConfig>
-		<Container fluid>
-			<Row className="justify-content-center align-items-center">
-				<Col md="auto" lg="4" xs="6">
+		<Container className="container-fluid">
+			<Row className="justify-content-center">
+				<Col md="auto" lg="6" md="6" xs="12">
 					<SearchText />
+					<WeatherBox />
 				</Col>
 			</Row>
 		</Container>
