1d8bef111ee856ae31b6f87719eed5406a56610e
Avner <avner.conceicao@hotmail.com>
2019-11-21 22:59:30 -0300

    Criando método 'changeWeatherUnit' em 'WeatherBox'

--- src/app/components/WeatherBox/WeatherBox.js
+++ src/app/components/WeatherBox/WeatherBox.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+
+//Actions
+import { SET_WEATHER_DATA, FAIL_SET_WEATHER_DATA } from '../../actions'
+
+//API
+import { getWeatherByCity } from '../../../api/WeatherAPI'
 
 //Components
 import WeatherDetails from './WeatherDetails'
@@ -24,76 +30,108 @@ const Box = styled.div`
 const Col = styled.div`
 	${props => props.background ? `background: rgba(${props.background},${props.opacity}) !important;`: ''}
 `
+class WeatherBox extends Component
+{
+	changeWeatherUnit = async (city, unit) => {
+		const { dispatch } = this.props
 
-const WeatherBox = props => {
-	const { data, error } = props
+		try {
+			const data = await getWeatherByCity(city, unit)
 
-	if (Object.keys(data).length === 0 || error) {
-		return (
-			<Box className="justify-content-center">
-				<Col className="col-12 weather-box" style={{
-					textAlign: 'center',
-					padding: '6rem 0',
-					fontWeight: 'bold',
-					color: 'red',
-					textTransform: 'capitalize'
-				}}>{error || "Nothing to Show"}</Col>		
-				<Col className="col-12 weather-box"></Col>
-				<Col className="col-12 weather-box"></Col>
-			</Box>	
-		)
+			if (data.error)
+				throw new Error(data.error)
+
+			dispatch({
+				type: SET_WEATHER_DATA,
+				payload: {
+					data
+				}
+			})
+		} catch(e) {
+			dispatch({
+				type: FAIL_SET_WEATHER_DATA,
+				payload: {
+					error: e.message
+				}
+			})
+		}
 	}
 
-	return (
-		<Box className="justify-content-center">
-			{data && data.list.map((item, index) => {
-				const temperature = item.main.temp
+	render() {
+		const { data, error } = this.props
 
-				let color = "255, 215, 0"
-				let opacity = ''
+		if (Object.keys(data).length === 0 || error) {
+			return (
+				<Box className="justify-content-center">
+					<Col className="col-12 weather-box" style={{
+						textAlign: 'center',
+						padding: '6rem 0',
+						fontWeight: 'bold',
+						color: 'red',
+						textTransform: 'capitalize'
+					}}>{error || "Nothing to Show"}</Col>		
+					<Col className="col-12 weather-box"></Col>
+					<Col className="col-12 weather-box"></Col>
+				</Box>	
+			)
+		}
 
-				if (temperature < 15) {
-					color = "70, 130, 180"
-					opacity = ""
-				} else if (temperature > 35) {
-					color = "255, 0, 0"
-				}
+		//Change unit
+		const changeUnit = data.city.unit === 'metric' ? 'imperial' : 'metric'
 
-				//Define opacity
-				switch (index + 1) {
-					case 1:
-						opacity = ".5"
-						break
-					case 3:
-						opacity = ".4"
-						break;				
-					default:
-						opacity = ".7"
-						break;
-				}
-	
-				return (
-					<Col
-						className="col-12 weather-box" 
-						key={index}
-						background={color}
-						opacity={opacity}>
-						
-						<WeatherDetails
-							index={index}
-							date={item.dt_txt} 
-							temperature={temperature}
-							description={item.weather[0].description}
-							wind={item.wind.speed}
-							humidity={item.main.humidity}
-							pressure={item.main.pressure} 
-							unit="C"
-							icon={item.weather[0].icon} />
-					</Col>
-				)
-			})}
-		</Box>
-	)
+		return (
+			<Box className="justify-content-center">
+				{data && data.list.map((item, index) => {
+					const temperature = item.main.temp
+					
+
+					let color = "255, 215, 0"
+					let opacity = ''
+
+					if (temperature < 15) {
+						color = "70, 130, 180"
+						opacity = ""
+					} else if (temperature > 35) {
+						color = "255, 0, 0"
+					}
+
+					//Define opacity
+					switch (index + 1) {
+						case 1:
+							opacity = ".5"
+							break
+						case 3:
+							opacity = ".4"
+							break;				
+						default:
+							opacity = ".7"
+							break;
+					}
+		
+					return (
+						<Col
+							className="col-12 weather-box" 
+							key={index}
+							background={color}
+							opacity={opacity}
+							onClick={_=>this.changeWeatherUnit(data.city.name, changeUnit)}>
+							
+							<WeatherDetails
+								index={index}
+								date={item.dt_txt} 
+								temperature={temperature}
+								description={item.weather[0].description}
+								wind={item.wind.speed}
+								humidity={item.main.humidity}
+								pressure={item.main.pressure} 
+								unit={data.city.unit === 'metric' ? 'C' : 'F'}
+								icon={item.weather[0].icon} />
+						</Col>
+					)
+				})}
+			</Box>
+		)
+	}
 }
 
 const { string, object } = PropTypes
